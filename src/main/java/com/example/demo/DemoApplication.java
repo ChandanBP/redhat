f171c414547668ab54b901df6c58bc2cd64e4ce8
“ChandanBP” <cbp698@gmail.com>
2020-10-10 12:41:26 +0530

    new changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
 import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+
+		int arr[] = {2,3,5};
+		int target = 10;
+
+		Arrays.sort(arr);
+		int possible=(int)Math.round(1.0*target/arr.length);
+		System.out.println(possible);
+		int low=0;
+		while(possible>arr[low]){
+			target-=arr[low];
+			if(++low==arr.length)break;
+			possible=(int)Math.round((1.0*target/(arr.length-low)-.1));
+		}
+		System.out.println(possible);
 	}
 
 }
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(value = "/")
 public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello,Welcome to smart shop. New experience in online shopping";
+    }
 }
